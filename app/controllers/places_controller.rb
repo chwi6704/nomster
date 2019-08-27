fa1190ba93437a3515f68c3ed7f791e12ecf9ea5
Christian Wigert <christian.wigert@gmail.com>
2019-08-27 03:27:09 +0000

    added routes for the user to enter data for name, address and description

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,11 +4,19 @@ def index
   @places = Place.all
 end
 
-def new
+ def new
+    @place = Place.new
   end
 
-  def new
-    @place = Place.new
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end
 
 end

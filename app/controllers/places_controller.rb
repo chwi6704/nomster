c241b3186720c90b2cf77d7dd57dc32eb0e70aad
Christian Wigert <christian.wigert@gmail.com>
2019-08-30 19:01:28 +0000

    added validators for name and editting places. Styled the error message

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 def index
   @places = Place.all
 end
@@ -10,7 +10,12 @@ end
 
   def create
     current_user.places.create(place_params)
+     @place = current_user.places.create(place_params)
+    if @place.valid?
     redirect_to root_path
+    else
+    render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -29,13 +34,21 @@ end
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.update_attributes(place_params)
+    if @place.valid?
     redirect_to root_path
+    else
+    render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end

8a4a576f15e848f59f4e8c0df2ec4714fe04c5ee
Christian Wigert <christian.wigert@gmail.com>
2019-09-08 19:13:26 +0000

    resolved double entry place issue by updatying places.rb file

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,13 +9,12 @@ end
   end
 
   def create
-    current_user.places.create(place_params)
-    @place = current_user.places.create(place_params)
+     @place = current_user.places.create(place_params)
   if @place.valid?
     redirect_to root_path
   else
     render :new, status: :unprocessable_entity
-    end
+  end
   end
 
   def show
